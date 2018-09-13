218771cf20ab2dce15a57289dcd7ab7bfd6aa95b
Oksana Shvets <oksana.shvets@coconutcalendar.com>
2018-09-13 14:39:44 -0600

    Added my JS study project folder

---
+++ javascripting/if-statement.js
@@ -0,0 +1,5 @@
+var fruit = 'orange';
+var fruitLength = fruit.length;
+if (fruitLength > 5) 
+	{console.log('The fruit name has more than five characters.')}
+else {console.log('The fruit name has five characters or less.')};
